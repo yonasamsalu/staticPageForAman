25ff16543534a3b976c9d3c5361fb199797c32bf
yonasamsalu <yonasamsalu75@gmail.com>
2024-11-05 17:30:42 +0300

    creating a "user" class model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    father_name = models.CharField(max_length=50)
+    grandfather_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    sex = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=15)
+
+
+def __str__(self):
+        return self.username
