dd86b5cf670e8c5d2d4fc81ce3b54eb75a016eeb
yonasamsalu <yonasamsalu75@gmail.com>
2024-11-05 22:53:34 +0300

    change a field name onforms.py

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,7 @@ from .models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'father_name', 'grandfather_name', 'email', 'sex', 'phone_number']
+        fields = ['your_name', 'father_name', 'grandfather_name', 'email', 'sex', 'phone_number']
         widgets = {
             'sex': forms.Select(choices=[('Male', 'Male'), ('Female', 'Female')]),
         }
