98e006d81591590c1d65680f5ca4e16355e8426d
yonasamsalu <yonasamsalu75@gmail.com>
2024-11-11 11:11:50 +0300

    remove password url

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('password/', views.password, name='password'),
-    path('home/', views.home, name='home'),  # Home page or next page after registration
+    path('home/', views.home, name='home'),
 ]
