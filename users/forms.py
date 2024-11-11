23fdc126c36bb3394fe580edc0d62bfa00ad2bba
yonasamsalu <yonasamsalu75@gmail.com>
2024-11-11 11:09:09 +0300

    insert additional fields into forms.py

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,16 @@ from .models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['your_name', 'father_name', 'grandfather_name', 'email', 'sex', 'phone_number']
+        fields = "__all__"
         widgets = {
-            'sex': forms.Select(choices=[('Male', 'Male'), ('Female', 'Female')]),
+            'dob_gregorian': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'dob_ethiopian': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'passport_issue_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'passport_expiry_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'your_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'father_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'grandfather_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'passport_number': forms.TextInput(attrs={'class': 'form-control'}),
         }
