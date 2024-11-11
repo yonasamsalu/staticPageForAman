e67a0dae7b9e38bbeae0334055f177f4bf245143
yonasamsalu <yonasamsalu75@gmail.com>
2024-11-11 11:07:03 +0300

    create a class on the admin.py file to display fields on the admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
+
 from django.contrib import admin
+from . models import User
+#from . import models
+from .forms import UserForm  # Import the custom form
 
 # Register your models here.
+
+
+@admin.register(User)
+class Useradmin(admin.ModelAdmin):
+    list_display = ['your_name', 'father_name', 'grandfather_name', 'sex','email', 'dob_ethiopian',
+                    'dob_gregorian', 'phone_number', 'passport_number', 'passport_issue_date', 'passport_expiry_date']
